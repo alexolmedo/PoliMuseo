573086ad00d30f99b9fde266168633f2144b4624
Andrés de la Torre <delatorre.an@gmail.com>
2018-02-18 15:14:33 -0500

    Añadido: - Lector Codigo QR - Informacion del Museo (no tiene contactos) - Navegación hacia atrás en Activities

--- app/src/main/java/com/fisepn/polimuseo/MainActivity.java
+++ app/src/main/java/com/fisepn/polimuseo/MainActivity.java
@@ -1,14 +1,20 @@
 package com.fisepn.polimuseo;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button btn_tourvirtual;
+    private ZXingScannerView vistaescaner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,4 +29,36 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    public  void Escanear (View view)
+    {
+        vistaescaner = new ZXingScannerView(this);
+        vistaescaner.setResultHandler( new xzingscanner());
+        setContentView(vistaescaner);
+        vistaescaner.startCamera();
+    }
+
+    class xzingscanner implements ZXingScannerView.ResultHandler
+    {
+        @Override
+        public void handleResult(Result result) {
+            String dato = result.getText();
+            setContentView(R.layout.activity_main);
+            vistaescaner.stopCamera();
+
+
+            Uri uri = Uri.parse(dato);
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(intent);
+        }
+    }
+
+    public void goInfoMuseo(View view) {
+        Intent intent = new Intent(this, InfoMuseoActivity.class);
+        startActivity(intent);
+    }
+
+
 }
+
+
